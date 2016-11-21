99082af6397cc72536ddf5f0a219d32977e0714b
dslanger <dslanger@gmail.com>
2016-11-20 22:37:35 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// 'use strict';
+//
+// angular.module('flickrSearcher', [])
+//   .controller('mainController', ['$http', function($http) {
+//     var vm = this;
+//     vm.getStuffFunction = function() {
+//       $http.get('')
+//       .then(function(response) {
+//
+//       },
+//       function(error) {
+//         console.log(error);
+//       });
+//     };
+//     vm.colors = [];
+//   }]);
